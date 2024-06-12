1a11fc6cea4094fcf3ca6cee8f19afdaa2400572
ShreyashSingh1 <shreyashsingh865@gmail.com>
2024-06-13 02:09:05 +0530

    feat: Add support for additional video memes
    The code changes include adding support for additional video memes. Two new template paths have been added for the videos "man_reaction_after_seeing_something_amazing" and "laughing_mischievously_devil_laugh". Additionally, a new video template "guy_lauging_and_saying_maja_aa_raha_hai" has been included. These changes allow users to generate a wider variety of video memes.
    
    This commit message follows the established convention of starting with a type prefix (`feat`) to indicate the nature of the changes. It provides a clear and concise description of the added functionality and the specific files affected.

--- application.py
+++ application.py
@@ -78,9 +78,11 @@ def video():
         "guy_is_dancing_three_person_are_laughing": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/laal_kabutar_meme_cat_is_dancing_three_cockroach_are_laughing_Oggy_and_cockroach.mp4",
         "modi_saying_kya_re_bete_tujhe_thakan_nhi_lag_rahi_kya": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/modi_saying_kya_re_bete_tujhe_thakan_nhi_lag_rahi_kya.mp4",
         "two_kids_are_running_after_seeing_something": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/two_kids_are_running_after_seeing_something.mp4",
-        "one_eternity_later":"C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/one_eternity_later.mp4",
         "man_shocked_after_seeing_something": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/guy_shocked_as_hell.mp4",
-        "man_shocked_after_listening_something": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/man_shocked_after_seeeing_something.mp4"
+        "man_reaction_after_seeing_something_amazing": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/man_shocked_after_seeeing_something.mp4",
+        "laughing_mischievously_devil_laugh": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/laughing_mischievously_devil_laugh.mp4",
+        "guy_lauging_and_saying_maja_aa_raha_hai": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/guy_lauging_and_saying_maja_aa_raha_hai.mp4",
+        "me_after_achieving_something_without_doing_noting": "C:/Users/shrey/OneDrive/Desktop/Memish/Vedio_Templates/me_after_achieving_something_without_doing_noting.mp4"
     }
     meme_gen1 = VedioGenerator("AIzaSyBa5b8ZuK83ehPi52ua4Ly724ofJHTT5Zk", template_paths)
     meme_gen1.create_video_meme(data["prompt"], "output_video.mp4")
