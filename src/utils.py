2b6da5f820af9424ba9e980f246f6996b5b64118
ShreyashSingh1 <shreyashsingh865@gmail.com>
2024-06-13 13:34:57 +0530

    feat: Add support for additional video memes
    The code changes include adding support for additional video memes. Two new template paths have been added for the videos "man_reaction_after_seeing_something_amazing" and "laughing_mischievously_devil_laugh". Additionally, a new video template "guy_lauging_and_saying_maja_aa_raha_hai" has been included. These changes allow users to generate a wider variety of video memes.

--- src/utils.py
+++ src/utils.py
@@ -62,3 +62,21 @@ def savenft1(path):
 template_paths = {
     "aerial_view_of_a_car_driving_down_a_road_in_the_middle_of_a_forest_1": "c:/Users/shrey/OneDrive/Desktop/Memish/notebooks/Templates/aerial_view_of_a_car_driving_down_a_road_in_the_middle_of_a_forest_1.jpg",
 }
+
+import os
+
+# Get the current working directory
+working_dir = os.getcwd()
+
+# Template paths dictionary with dynamic paths based on the working directory
+template_video_paths = {
+    "guy_is_dancing_three_person_are_laughing": os.path.join(working_dir, "video_templates/m1.mp4"),
+    "modi_saying_kya_re_bete_tujhe_thakan_nhi_lag_rahi_kya": os.path.join(working_dir, "video_templates/m2.mp4"),
+    "two_kids_are_running_after_seeing_something": os.path.join(working_dir, "video_templates/m3.mp4"),
+    "man_shocked_after_seeing_something": os.path.join(working_dir, "video_templates/m4.mp4"),
+    "man_reaction_after_seeing_something_amazing_good": os.path.join(working_dir, "video_templates/m5.mp4"),
+    "laughing_mischievously_devil_laugh": os.path.join(working_dir, "video_templates/m6.mp4"),
+    "guy_lauging_and_saying_maja_aa_raha_hai": os.path.join(working_dir, "video_templates/m7.mp4"),
+    "after_achieving_something_without_doing_noting": os.path.join(working_dir, "video_templates/m8.mp4"),
+    "guy_loosing_his_mental_stability": os.path.join(working_dir, "video_templates/m9.mp4")
+}
